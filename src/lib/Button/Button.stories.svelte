9599e053aa2a88d920858465a7078a51ddd657db
Joern Bernhardt <joern.bernhardt@compose.us>
2021-10-29 00:51:43 +0200

    Add example how to use slots
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/Button/Button.stories.svelte
+++ src/lib/Button/Button.stories.svelte
@@ -1,19 +1,31 @@
 <script>
 	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { action } from '@storybook/addon-actions';
 	import Button from './Button.svelte';
 </script>
 
 <Meta title="Components/Button" component={Button} />
 
-<Template let:args>
-	<Button {...args} on:click={args.onClick}><slot /></Button>
+<Template id="test" let:args>
+	<Button on:click={args.onClick}>Test</Button>
+</Template>
+
+<Template id="hello" let:args>
+	<Button on:click={args.onClick}>Hello</Button>
 </Template>
 
 <Story
 	name="Primary"
+  template="test"
 	args={{
-		onClick: () => {
-			console.log('clicked');
-		}
-	}}>Button</Story
->
+		onClick: action('clicked')
+	}}
+/>
+
+<Story
+	name="Second"
+  template="hello"
+	args={{
+		onClick: action('clicked')
+	}}
+/>
--- src/lib/Button/Button.svelte
+++ src/lib/Button/Button.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	export let onClick: () => void;
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher<{ click: string }>();
+
+	function onClick() {
+		console.log('clicked?');
+		dispatch('click', 'hello');
+	}
 </script>
 
 <button on:click={onClick}><slot /></button>
 
 <style>
 	button {
+		border: 1px solid #000;
 		padding: 8px 24px;
 	}
 </style>
