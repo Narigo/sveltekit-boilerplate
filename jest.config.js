973513baf6e1629786b594a1ac16eba61965775e
Joern Bernhardt <joern.bernhardt@compose.us>
2021-11-01 01:42:30 +0100

    Try using jsdom and test button component
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 export default {
+	moduleFileExtensions: ['js', 'svelte', 'ts'],
+	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 	transform: {
 		'^.+\\.svelte$': ['svelte-jester', { preprocess: true }],
 		'^.+\\.ts$': 'ts-jest'
-	},
-	moduleFileExtensions: ['js', 'svelte', 'ts']
+	}
 };
--- src/lib/Button/Button.spec.ts
+++ src/lib/Button/Button.spec.ts
@@ -1,7 +1,13 @@
-import Button from './Button.svelte';
+import ButtonTest from './ButtonTest.svelte';
+import { render } from '@testing-library/svelte';
+
+/**
+ * @jest-environment jsdom
+ */
 
 describe('Button', () => {
 	it('has a working test setup', () => {
-		expect(true).toBe(true);
+		const { getByText } = render(ButtonTest);
+		expect(getByText('Hello World')).not.toBeNull();
 	});
 });
