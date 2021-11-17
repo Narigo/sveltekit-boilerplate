f76a35179e11ed07ff16f1af50f8815578311fbf
Joern Bernhardt <joern.bernhardt@compose.us>
2021-11-17 20:22:08 +0100

    Fix storybook link from svelte
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Check <a href="/storybook/index.html">Storybook</a> components.</p>
+<p>Check <a href="/storybook/" rel="external">Storybook</a> components.</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapterStatic(),
+		// adapter: adapterStatic(),
+		adapter: adapterStatic({
+			fallback: 'index.html'
+		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
