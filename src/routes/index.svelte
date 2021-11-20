3cd8b1ecd4ada4bbb36009cb96df0b326fdb50c1
Joern Bernhardt <joern.bernhardt@compose.us>
2021-11-20 23:48:46 +0100

    Add link to sources
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Check <a href="/storybook/" rel="external">Storybook</a> components.</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Inspect the <a href="https://github.com/Narigo/sveltekit-boilerplate"
+		>source code of this boilerplate at GitHub</a
+	>.
+</p>
