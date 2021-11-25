f08590c3a93f0ac9d9a7ecca846b08b30641b6b2
Joern Bernhardt <joern.bernhardt@compose.us>
2021-11-25 20:37:28 +0100

    Rename for correct intent, make it use the same app for storybook runs
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- cy-open.sh
+++ cy-open.sh
@@ -5,4 +5,4 @@ open -a XQuartz
 IP=$(ipconfig getifaddr en0)
 /usr/X11/bin/xhost + $IP
 
-DISPLAY=${IP}:0 docker compose -f docker-compose.yml -f cypress/docker-compose.cy-open.yml up --exit-code-from cypress $@
+DISPLAY=${IP}:0 docker compose -f docker-compose.yml -f cypress/docker-compose.cy-storybook-open.yml up --exit-code-from app $@
