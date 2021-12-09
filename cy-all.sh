527fe8d1ff60e4e91cb5f54db126dc1e219416c3
Joern Bernhardt <joern.bernhardt@compose.us>
2021-12-10 00:05:15 +0100

    Make sure cy-all works with latest versions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- cy-all.sh
+++ cy-all.sh
@@ -5,4 +5,4 @@ open -a XQuartz
 IP=$(ipconfig getifaddr en0)
 /usr/X11/bin/xhost + $IP
 
-DISPLAY=${IP}:0 docker compose -f docker-compose.yml -f cypress/docker-compose.cypress.yml run app
+DISPLAY=${IP}:0 docker compose -f docker-compose.yml -f cypress/docker-compose.cypress.yml run --service-ports app
