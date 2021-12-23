31d60404290eeccb15fd44e34ff2b9d155b5a095
Joern Bernhardt <joern.bernhardt@compose.us>
2021-12-24 00:01:48 +0100

    fix: Code style and waiting for click
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/Button/Button.integration.ts
+++ src/lib/Button/Button.integration.ts
@@ -4,11 +4,14 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Button', () => {
 	test('can be clicked', async ({ page }) => {
-		await page.goto('http://localhost:6006/iframe.html?id=components-button--regular&viewMode=story', { waitUntil: 'networkidle' });
-		const button = page.locator('#root > button')
+		await page.goto(
+			'http://localhost:6006/iframe.html?id=components-button--regular&viewMode=story',
+			{ waitUntil: 'networkidle' }
+		);
+		const button = page.locator('#root > button');
 		await button.waitFor();
 		await expect(button).toHaveText('Button');
-		await page.click('button');
+		page.click('button');
 		await expect(true).toBe(true);
 	});
 });
