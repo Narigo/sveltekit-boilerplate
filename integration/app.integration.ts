61419a81450b2d4095bcc5f7bdaa0183410399ab
Joern Bernhardt <joern.bernhardt@compose.us>
2021-12-24 01:01:46 +0100

    fix: Fix test
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- integration/app.integration.ts
+++ integration/app.integration.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
 	await page.goto('http://localhost:3000/');
 	const title = page.locator('h1');
-	await expect(title).toHaveText('SvelteKit');
+	await expect(title).toContainText('SvelteKit');
 });
