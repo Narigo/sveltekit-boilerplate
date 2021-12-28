643007cbcd240fb82b47ec454d6d2d09baa1183c
Joern Bernhardt <joern.bernhardt@compose.us>
2021-12-28 23:27:11 +0100

    fix: Add rel=external to all links, remove stray links
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,44 +1,50 @@
 <h1>SvelteKit boilerplate</h1>
 <p>This boilerplate features:</p>
-https://kit.svelte.dev/ https://storybook.js.org/ https://www.typescriptlang.org/ https://prettier.io/
-#running-checks https://eslint.org/ #running-unit-tests-jest https://jestjs.io/ https://testing-library.com/
-#running-browser-tests-playwright https://playwright.dev/ .github/workflows/run-checks.yml .github/workflows/publish-docs.yml
 <ul>
-	<li>- <a href="https://kit.svelte.dev/">SvelteKit</a>(https://kit.svelte.dev/)</li>
+	<li>- <a href="https://kit.svelte.dev/" rel="external">SvelteKit</a>(https://kit.svelte.dev/)</li>
 	<li>
-		- component based development (<a href="https://storybook.js.org/">Storybook</a>)
+		- component based development (<a href="https://storybook.js.org/" rel="external">Storybook</a>)
 	</li>
 	<li>
-		- type-safety (<a href="https://www.typescriptlang.org/">TypeScript</a>)
+		- type-safety (<a href="https://www.typescriptlang.org/" rel="external">TypeScript</a>)
 	</li>
-	<li>- auto-formatting (<a href="https://prettier.io/">prettier</a>)</li>
+	<li>- auto-formatting (<a href="https://prettier.io/" rel="external">prettier</a>)</li>
 	<li>
-		- <a href="#running-checks">code-linting</a> (<a href="https://eslint.org/">eslint</a>)
+		- <a href="#running-checks" rel="external">code-linting</a> (<a
+			href="https://eslint.org/"
+			rel="external">eslint</a
+		>)
 	</li>
 	<li>
-		- <a href="#running-unit-tests-jest">unit and component tests</a> (<a href="https://jestjs.io/"
-			>Jest</a
+		- <a href="#running-unit-tests-jest" rel="external">unit and component tests</a> (<a
+			href="https://jestjs.io/"
+			rel="external">Jest</a
 		>
 		with
-		<a href="https://testing-library.com/">testing-library</a>)
+		<a href="https://testing-library.com/" rel="external">testing-library</a>)
 	</li>
 	<li>
-		- <a href="#running-browser-tests-playwright">browser tests</a> (<a
-			href="https://playwright.dev/">Playwright</a
+		- <a href="#running-browser-tests-playwright" rel="external">browser tests</a> (<a
+			href="https://playwright.dev/"
+			rel="external">Playwright</a
 		> for Storybook and integration)
 	</li>
 	<li>
-		- <a href=".github/workflows/run-checks.yml">CI tests for pull requests</a> (GitHub actions)
+		- <a href=".github/workflows/run-checks.yml" rel="external">CI tests for pull requests</a> (GitHub
+		actions)
 	</li>
 	<li>
-		- <a href=".github/workflows/publish-docs.yml">auto-deployment for static pages and Storybook</a
+		- <a href=".github/workflows/publish-docs.yml" rel="external"
+			>auto-deployment for static pages and Storybook</a
 		> (on GitHub pages)
 	</li>
 </ul>
 <p>Check <a href="/storybook/" rel="external">Storybook</a> components.</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>
-	Inspect the <a href="https://github.com/Narigo/sveltekit-boilerplate"
+	Visit <a href="https://kit.svelte.dev" rel="external">kit.svelte.dev</a> to read the documentation
+</p>
+<p>
+	Inspect the <a href="https://github.com/Narigo/sveltekit-boilerplate" rel="external"
 		>source code of this boilerplate at GitHub</a
 	>.
 </p>
