f195231afc16dc74326a7093a07eafb32646d7f9
Joern Bernhardt <joern.bernhardt@compose.us>
2021-12-28 23:23:44 +0100

    feat: Add boilerplate features to index page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,40 @@
 <h1>SvelteKit boilerplate</h1>
+<p>This boilerplate features:</p>
+https://kit.svelte.dev/ https://storybook.js.org/ https://www.typescriptlang.org/ https://prettier.io/
+#running-checks https://eslint.org/ #running-unit-tests-jest https://jestjs.io/ https://testing-library.com/
+#running-browser-tests-playwright https://playwright.dev/ .github/workflows/run-checks.yml .github/workflows/publish-docs.yml
+<ul>
+	<li>- <a href="https://kit.svelte.dev/">SvelteKit</a>(https://kit.svelte.dev/)</li>
+	<li>
+		- component based development (<a href="https://storybook.js.org/">Storybook</a>)
+	</li>
+	<li>
+		- type-safety (<a href="https://www.typescriptlang.org/">TypeScript</a>)
+	</li>
+	<li>- auto-formatting (<a href="https://prettier.io/">prettier</a>)</li>
+	<li>
+		- <a href="#running-checks">code-linting</a> (<a href="https://eslint.org/">eslint</a>)
+	</li>
+	<li>
+		- <a href="#running-unit-tests-jest">unit and component tests</a> (<a href="https://jestjs.io/"
+			>Jest</a
+		>
+		with
+		<a href="https://testing-library.com/">testing-library</a>)
+	</li>
+	<li>
+		- <a href="#running-browser-tests-playwright">browser tests</a> (<a
+			href="https://playwright.dev/">Playwright</a
+		> for Storybook and integration)
+	</li>
+	<li>
+		- <a href=".github/workflows/run-checks.yml">CI tests for pull requests</a> (GitHub actions)
+	</li>
+	<li>
+		- <a href=".github/workflows/publish-docs.yml">auto-deployment for static pages and Storybook</a
+		> (on GitHub pages)
+	</li>
+</ul>
 <p>Check <a href="/storybook/" rel="external">Storybook</a> components.</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>
