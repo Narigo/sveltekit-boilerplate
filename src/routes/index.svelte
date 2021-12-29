db638d2c3d8eaaf2c27d7c824260113704d66f4f
Joern Bernhardt <joern.bernhardt@compose.us>
2021-12-29 01:25:02 +0100

    fix: Fix links in index
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	const repoUrl = 'https://github.com/Narigo/sveltekit-boilerplate';
+</script>
+
 <h1>SvelteKit boilerplate</h1>
 <p>This boilerplate features:</p>
 <ul>
@@ -10,31 +14,31 @@
 	</li>
 	<li>- auto-formatting (<a href="https://prettier.io/" rel="external">prettier</a>)</li>
 	<li>
-		- <a href="#running-checks" rel="external">code-linting</a> (<a
+		- <a href="{repoUrl}/README.md#running-checks" rel="external">code-linting</a> (<a
 			href="https://eslint.org/"
 			rel="external">eslint</a
 		>)
 	</li>
 	<li>
-		- <a href="#running-unit-tests-jest" rel="external">unit and component tests</a> (<a
-			href="https://jestjs.io/"
-			rel="external">Jest</a
+		- <a href="{repoUrl}/README.md#running-unit-tests-jest" rel="external"
+			>unit and component tests</a
 		>
+		(<a href="https://jestjs.io/" rel="external">Jest</a>
 		with
 		<a href="https://testing-library.com/" rel="external">testing-library</a>)
 	</li>
 	<li>
-		- <a href="#running-browser-tests-playwright" rel="external">browser tests</a> (<a
-			href="https://playwright.dev/"
-			rel="external">Playwright</a
-		> for Storybook and integration)
+		- <a href="{repoUrl}/README.md#running-browser-tests-playwright" rel="external">browser tests</a
+		>
+		(<a href="https://playwright.dev/" rel="external">Playwright</a> for Storybook and integration)
 	</li>
 	<li>
-		- <a href=".github/workflows/run-checks.yml" rel="external">CI tests for pull requests</a> (GitHub
-		actions)
+		- <a href="{repoUrl}/.github/workflows/run-checks.yml" rel="external"
+			>CI tests for pull requests</a
+		> (GitHub actions)
 	</li>
 	<li>
-		- <a href=".github/workflows/publish-docs.yml" rel="external"
+		- <a href="{repoUrl}/.github/workflows/publish-docs.yml" rel="external"
 			>auto-deployment for static pages and Storybook</a
 		> (on GitHub pages)
 	</li>
@@ -44,7 +48,5 @@
 	Visit <a href="https://kit.svelte.dev" rel="external">kit.svelte.dev</a> to read the documentation
 </p>
 <p>
-	Inspect the <a href="https://github.com/Narigo/sveltekit-boilerplate" rel="external"
-		>source code of this boilerplate at GitHub</a
-	>.
+	Inspect the <a href={repoUrl} rel="external">source code of this boilerplate at GitHub</a>.
 </p>
