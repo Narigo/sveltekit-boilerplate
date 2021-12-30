a2e3b9f0745161ee745ef4f6e7d4fc88bed63f31
Joern Bernhardt <joern.bernhardt@compose.us>
2021-12-30 01:25:09 +0100

    feat: Link to index page of repository if possible
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	const repoUrl = 'https://github.com/Narigo/sveltekit-boilerplate/blob/main';
+	const gitHubUrl = 'https://github.com/Narigo/sveltekit-boilerplate';
+	const repoUrl = `${gitHubUrl}/blob/main`;
 </script>
 
 <h1>SvelteKit boilerplate</h1>
@@ -14,22 +15,19 @@
 	</li>
 	<li>- auto-formatting (<a href="https://prettier.io/" rel="external">prettier</a>)</li>
 	<li>
-		- <a href="{repoUrl}/README.md#running-checks" rel="external">code-linting</a> (<a
+		- <a href="{gitHubUrl}#running-checks" rel="external">code-linting</a> (<a
 			href="https://eslint.org/"
 			rel="external">eslint</a
 		>)
 	</li>
 	<li>
-		- <a href="{repoUrl}/README.md#running-unit-tests-jest" rel="external"
-			>unit and component tests</a
-		>
+		- <a href="{gitHubUrl}#running-unit-tests-jest" rel="external">unit and component tests</a>
 		(<a href="https://jestjs.io/" rel="external">Jest</a>
 		with
 		<a href="https://testing-library.com/" rel="external">testing-library</a>)
 	</li>
 	<li>
-		- <a href="{repoUrl}/README.md#running-browser-tests-playwright" rel="external">browser tests</a
-		>
+		- <a href="{gitHubUrl}#running-browser-tests-playwright" rel="external">browser tests</a>
 		(<a href="https://playwright.dev/" rel="external">Playwright</a> for Storybook and integration)
 	</li>
 	<li>
@@ -48,5 +46,5 @@
 	Visit <a href="https://kit.svelte.dev" rel="external">kit.svelte.dev</a> to read the documentation
 </p>
 <p>
-	Inspect the <a href={repoUrl} rel="external">source code of this boilerplate at GitHub</a>.
+	Inspect the <a href={gitHubUrl} rel="external">source code of this boilerplate at GitHub</a>.
 </p>
